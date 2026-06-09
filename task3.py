8df121b543a0111e9c1e869c67f688eff23ba3a7
Никита Гусаков <nikita.gusakov2010@gmail.com>
2026-06-09 13:29:28 +0300

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,2 @@
+num1,num2,num3,num4 = int(input()),int(input()),int(input()),int(input())#ввод всех 4 чисел
+print(max(num1,num2,num3,num4))#с пмощью служебной функции print и msx выводим максимальное число
